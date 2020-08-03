cb57c4b5719f8fb2a4ef2360c48cddf293948f1c
DavidStinson <david.stinson.5@gmail.com>
2020-08-03 07:58:00 -0500

    add border box

--- js/main.js
+++ js/main.js
@@ -16,3 +16,14 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
+class Shape {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  static getPosition() {
+    return [this.x, this.y];
+  }
+}
+
+console.log(Shape.getPosition())
